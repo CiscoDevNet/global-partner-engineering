abf5f4942e09bf6e433a0fb190c58c47e9ef1aa0
Joel Jose <11584709+joeljos@users.noreply.github.com>
2024-09-25 06:43:53 +0530

    Update credentials.py

--- AI enabled Outcomes/Outcome 1/Dashboard/credentials.py
+++ AI enabled Outcomes/Outcome 1/Dashboard/credentials.py
@@ -5,7 +5,7 @@ DNAC_PASSWORD = 'Guest123'
 vManage_username = 'guest'
 vManage_password = 'Guest123'
 
-thousandEyes_token = "ff63a91e-917c-4bd6-8579-9b2e17226589"
+thousandEyes_token = ""
 
-mongodb_uri = "mongodb+srv://admin:zPSWowDsZgD6a0Mt@noc.5i3t1qh.mongodb.net/?retryWrites=true&w=majority"
+mongodb_uri = ""
 
